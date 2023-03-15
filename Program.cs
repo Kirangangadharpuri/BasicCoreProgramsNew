170714b4648c2d33d004a17f3a9d17340d3fcc01
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-03-14 17:32:45 -1200

    Welcome page

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+            
+        }
+    }
+}
